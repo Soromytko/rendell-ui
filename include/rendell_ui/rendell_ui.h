12221f421cc522ddcc21741e4103de9b0f368e91
Soromytko <NikitaSoromytko@gmail.com>
2025-01-24 15:11:08 +0300

    Add CMakeLists.txt file and include directory

---
+++ include/rendell_ui/rendell_ui.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "../../src/initialization.h"
+#include "../../src/Window.h"
+#include "../../src/Viewport.h"
+
+#include "../../src/Widgets/Rectangle.h"
+#include "../../src/Widgets/Text.h"
+
