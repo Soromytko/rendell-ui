1bff0e7917d6849eee52f356dbbaebe639a7a974
Soromytko <NikitaSoromytko@gmail.com>
2025-06-24 01:13:00 +0300

    Fix for Linux build

--- internal/logging.h
+++ internal/logging.h
@@ -20,9 +20,9 @@ namespace rendell_ui
 
 }
 
-#define RUI_CRITICAL(formatStr, ...) rendell_ui::get_logger()->critical(std::format(formatStr, __VA_ARGS__))
-#define RUI_ERROR(formatStr, ...) rendell_ui::get_logger()->error(std::format(formatStr, __VA_ARGS__))
-#define RUI_WARNING(formatStr, ...) rendell_ui::get_logger()->warning(std::format(formatStr, __VA_ARGS__))
-#define RUI_INFO(formatStr, ...) rendell_ui::get_logger()->info(std::format(formatStr, __VA_ARGS__))
-#define RUI_DEBUG(formatStr, ...) rendell_ui::get_logger()->debug(std::format(formatStr, __VA_ARGS__))
-#define RUI_TRACE(formatStr, ...) rendell_ui::get_logger()->trace(std::format(formatStr, __VA_ARGS__))
+#define RUI_CRITICAL(formatStr, ...) rendell_ui::get_logger()->critical(std::format(formatStr, ##__VA_ARGS__))
+#define RUI_ERROR(formatStr, ...) rendell_ui::get_logger()->error(std::format(formatStr, ##__VA_ARGS__))
+#define RUI_WARNING(formatStr, ...) rendell_ui::get_logger()->warning(std::format(formatStr, ##__VA_ARGS__))
+#define RUI_INFO(formatStr, ...) rendell_ui::get_logger()->info(std::format(formatStr, ##__VA_ARGS__))
+#define RUI_DEBUG(formatStr, ...) rendell_ui::get_logger()->debug(std::format(formatStr, ##__VA_ARGS__))
+#define RUI_TRACE(formatStr, ...) rendell_ui::get_logger()->trace(std::format(formatStr, ##__VA_ARGS__))
